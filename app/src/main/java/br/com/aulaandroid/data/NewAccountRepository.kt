437828f5342b1783d6e12c80749ca470cf5700fc
Daniel Eykel <danielbh609@hotmail.com>
2024-10-19 11:37:06 -0300

    Remoção de frase

--- app/src/main/java/br/com/aulaandroid/data/NewAccountRepository.kt
+++ app/src/main/java/br/com/aulaandroid/data/NewAccountRepository.kt
@@ -5,6 +5,4 @@ import br.com.aulaandroid.util.RequestHandler
 interface NewAccountRepository {
 
     suspend fun newAccount(email: String, password: String) : RequestHandler
-
-    //Vou adicionar de novo um comentário
 }
