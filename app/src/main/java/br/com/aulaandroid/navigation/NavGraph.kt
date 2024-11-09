61ffc4ae1367c5e7441f9a2f22f619a2f14706ce
Daniel Eykel <danielbh609@hotmail.com>
2024-11-09 12:01:30 -0300

    treatment the state on view and loading on button

--- app/src/main/java/br/com/aulaandroid/navigation/NavGraph.kt
+++ app/src/main/java/br/com/aulaandroid/navigation/NavGraph.kt
@@ -2,8 +2,17 @@ package br.com.aulaandroid.navigation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,29 +20,71 @@ import androidx.navigation.compose.rememberNavController
 import br.com.aulaandroid.ui.login.LoginScreen
 import br.com.aulaandroid.ui.newAccount.NewAccountScreen
 import br.com.aulaandroid.ui.theme.AulaAndroidTheme
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavGraph(){
     val navController = rememberNavController()
+    val scope = rememberCoroutineScope()
+    var bottomSheetIsOpen = rememberSaveable { mutableStateOf(false) }
+    val errorMessage = remember { mutableStateOf("") }
+
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+
+
 
     AulaAndroidTheme {
         Scaffold { innerPadding ->
             Column(Modifier.padding(innerPadding)) {
                 NavHost(navController, startDestination = Route.LoginScreen) {
                     composable<Route.LoginScreen> {
-                        LoginScreen(
-                            koinViewModel()
-                        ) { navController.navigate(it)  }
+                        LoginScreen(koinViewModel()){ state->
+                            when(state){
+                                is AulaAndroidState.Error -> {
+                                    errorMessage.value = state.message
+                                    bottomSheetIsOpen.value = true
+                                }
+                                is AulaAndroidState.Navigate -> navController.navigate(state.route)
+                            }
+                        }
                     }
 
                     composable<Route.NewAccountScreen> {
-                        NewAccountScreen(koinViewModel())
+                        NewAccountScreen(koinViewModel()) { state ->
+                            when(state){
+                                is AulaAndroidState.Error -> {
+                                    errorMessage.value = state.message
+                                    scope.launch { sheetState.show() }
+                                    bottomSheetIsOpen.value = true
+                                }
+                                is AulaAndroidState.Navigate -> navController.navigate(state.route)
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (bottomSheetIsOpen.value) {
+                ModalBottomSheet(
+                    onDismissRequest = {
+                        bottomSheetIsOpen.value = false
+                    },
+                    sheetState = sheetState
+                ) {
+                    // Sheet content
+                    Text(errorMessage.value)
+                    Button(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            bottomSheetIsOpen.value = false
+                        }
+                    }) {
+                        Text("Hide bottom sheet")
                     }
                 }
             }
         }
     }
-
-
 }
