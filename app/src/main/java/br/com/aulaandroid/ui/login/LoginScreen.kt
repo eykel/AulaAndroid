56d7f80c87e07687a9795e6e142c969b19d43aa6
Daniel Eykel <danielbh609@hotmail.com>
2024-11-09 12:02:36 -0300

    refactor of LoginScreen

--- app/src/main/java/br/com/aulaandroid/ui/login/LoginScreen.kt
+++ app/src/main/java/br/com/aulaandroid/ui/login/LoginScreen.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -33,114 +31,115 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.aulaandroid.R
+import br.com.aulaandroid.navigation.AulaAndroidState
 import br.com.aulaandroid.navigation.Route
-import br.com.aulaandroid.ui.theme.AulaAndroidTheme
-import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun LoginScreen(viewModel: LoginViewModel, maria: (route: Route) -> Unit) {
+fun LoginScreen(
+    viewModel: LoginViewModel,
+    onEvent: (AulaAndroidState) -> Unit,
+) {
+    Content(viewModel, onEvent)
+}
+
+@Composable
+private fun Content(viewModel: LoginViewModel, onEvent: (AulaAndroidState) -> Unit){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordVisible by rememberSaveable { mutableStateOf(false) }
     val validEmail by viewModel.validEmail.collectAsState()
     val register by viewModel.register.collectAsState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .paint(
+                painter = painterResource(id = R.drawable.img),
+                contentScale = ContentScale.Crop
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
-    if(register){
-        Text(text = "O USUÁRIO FOI REGISTRADO")
-    }else {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .paint(
-                    painter = painterResource(id = R.drawable.img),
-                    contentScale = ContentScale.Crop
-                ),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+        Text(text = "Meu APP", Modifier.padding(20.dp))
 
-            ) {
-            Text(text = "Meu APP", Modifier.padding(20.dp))
-
-            TextField(
-                modifier = Modifier.padding(20.dp),
-                value = email,
-                onValueChange = { newValue ->
-                    email = newValue
-                    viewModel.validEmail(email)
-                },
-                label = { Text(text = "Email") },
-                placeholder = { Text(text = "Email") },
-                isError = !validEmail,
-                colors = TextFieldDefaults.colors(
-                    focusedTextColor = Color.Blue,
-                )
+        TextField(
+            modifier = Modifier.padding(20.dp),
+            value = email,
+            onValueChange = { newValue ->
+                email = newValue
+                viewModel.validEmail(email)
+            },
+            label = { Text(text = "Email") },
+            placeholder = { Text(text = "Email") },
+            isError = !validEmail,
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color.Blue,
             )
+        )
 
 
-            TextField(
-                value = password,
-                onValueChange = { newValuer ->
-                    password = newValuer
+        TextField(
+            value = password,
+            onValueChange = { newValuer ->
+                password = newValuer
 
 
-                },
-                singleLine = true,
-                label = { Text(text = "Senha") },
-                placeholder = { Text(text = "Senha") },
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                trailingIcon = {
-                    val image = if (passwordVisible)
-                        Icons.Filled.Lock
-                    else Icons.Filled.Lock
+            },
+            singleLine = true,
+            label = { Text(text = "Senha") },
+            placeholder = { Text(text = "Senha") },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (passwordVisible)
+                    Icons.Filled.Lock
+                else Icons.Filled.Lock
 
-                    val description = if (passwordVisible) "Hide password" else "Show password"
+                val description = if (passwordVisible) "Hide password" else "Show password"
 
-                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                        Icon(imageVector = image, description)
-                    }
-                },
-                colors = TextFieldDefaults.colors(
-                    errorIndicatorColor = Color.Blue,
-                    focusedTextColor = Color.Blue
-                )
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = image, description)
+                }
+            },
+            colors = TextFieldDefaults.colors(
+                errorIndicatorColor = Color.Blue,
+                focusedTextColor = Color.Blue
             )
+        )
 
-            Button(
-                modifier = Modifier.padding(10.dp),
-                onClick = {
-                    //navigate.invoke()
-                }
-            ) {
-                Text(
-                    text = "Login",
-                )
+        Button(
+            modifier = Modifier.padding(10.dp),
+            onClick = {
+                //navigate.invoke()
             }
+        ) {
+            Text(
+                text = "Login",
+            )
+        }
 
-            TextButton(
-                onClick = { /*TODO*/ }
-            ) {
-                Text(
-                    color = Color.White,
-                    textDecoration = TextDecoration.Underline,
-                    text = "Se esqueceu sua senha, clique aqui!"
-                )
-            }
+        TextButton(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                color = Color.White,
+                textDecoration = TextDecoration.Underline,
+                text = "Se esqueceu sua senha, clique aqui!"
+            )
+        }
 
-            TextButton(
-                onClick = {
-                    maria.invoke(Route.NewAccountScreen)
-                }
-            ) {
-                Text(
-                    color = Color.White,
-                    textDecoration = TextDecoration.Underline,
-                    text = "Não possui conta? Cadastrar-se"
-                )
+        TextButton(
+            onClick = {
+                onEvent.invoke(AulaAndroidState.Navigate(Route.NewAccountScreen))
             }
+        ) {
+            Text(
+                color = Color.White,
+                textDecoration = TextDecoration.Underline,
+                text = "Não possui conta? Cadastrar-se"
+            )
         }
     }
 }
