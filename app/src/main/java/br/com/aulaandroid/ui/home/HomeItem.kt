e87c5418df94949a3cc5213f0a7956a42af7a4c3
Daniel Eykel <danielbh609@hotmail.com>
2025-03-08 11:31:44 -0300

    feat: remove ununsed import

--- app/src/main/java/br/com/aulaandroid/ui/home/HomeItem.kt
+++ app/src/main/java/br/com/aulaandroid/ui/home/HomeItem.kt
@@ -1,6 +1,5 @@
 package br.com.aulaandroid.ui.home
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
