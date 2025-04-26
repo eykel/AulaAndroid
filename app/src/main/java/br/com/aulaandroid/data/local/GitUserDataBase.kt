d561489633fbc0c8932d1e1dc18e0d8e0b66c925
Daniel Eykel <danielbh609@hotmail.com>
2025-04-26 12:06:46 -0300

    feat: Create and initialize Room database for GitUser
    -   Create `GitUserDataBase` with `DATABASE_NAME` constant.
    -   Initialize the database instance using `Room.databaseBuilder` with application context.
    - Add synchronized block to avoid concurrency issues.

--- app/src/main/java/br/com/aulaandroid/data/local/GitUserDataBase.kt
+++ app/src/main/java/br/com/aulaandroid/data/local/GitUserDataBase.kt
@@ -2,6 +2,7 @@ package br.com.aulaandroid.data.local
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import br.com.aulaandroid.data.local.dao.UserDAO
 import br.com.aulaandroid.data.model.GithubUser
@@ -12,11 +13,20 @@ abstract class GitUserDataBase : RoomDatabase(){
     abstract fun userDao(): UserDAO
 
     companion object {
+        const val DATABASE_NAME = "github_user_db"
+
+        @Volatile
         private lateinit var INSTANCE: GitUserDataBase
 
         fun getDatabase(context: Context): GitUserDataBase {
             if (!::INSTANCE.isInitialized) {
-                 //DATA BASE SERA CRIADO AQUI
+                 synchronized(this) {
+                     INSTANCE = Room.databaseBuilder(
+                         context.applicationContext,
+                         GitUserDataBase::class.java,
+                         DATABASE_NAME
+                     ).build()
+                 }
             }
             return INSTANCE
         }
