e707cfa95f46dd18734b14ee8edc8a5efcdfb2eb
Daniel Eykel <danielbh609@hotmail.com>
2025-05-03 11:51:07 -0300

    feat: Add favorite list functionality to GithubRepository
    - Implement `getFavoriteList` in `GithubRepositoryImpl` to retrieve favorite users from the local database.
    - Handle potential exceptions during database access and wrap the result in a `RequestHandler`.
    - Add the `getFavoriteList` function to the `GithubRepository` interface.

--- app/src/main/java/br/com/aulaandroid/data/repository/GithubRepository.kt
+++ app/src/main/java/br/com/aulaandroid/data/repository/GithubRepository.kt
@@ -7,4 +7,6 @@ interface GithubRepository {
     suspend fun getUserList(param: String) : RequestHandler
 
     suspend fun getUserDetail(login: String): RequestHandler
+
+    suspend fun getFavoriteList() : RequestHandler
 }
--- app/src/main/java/br/com/aulaandroid/data/repository/impl/GithubRepositoryImpl.kt
+++ app/src/main/java/br/com/aulaandroid/data/repository/impl/GithubRepositoryImpl.kt
@@ -1,12 +1,27 @@
 package br.com.aulaandroid.data.repository.impl
 
+import br.com.aulaandroid.data.local.dao.UserDAO
 import br.com.aulaandroid.data.networking.GithubNetworking
 import br.com.aulaandroid.data.repository.GithubRepository
 import br.com.aulaandroid.util.RequestHandler
 
-class GithubRepositoryImpl(val networking: GithubNetworking) : GithubRepository {
+class GithubRepositoryImpl(
+    val networking: GithubNetworking,
+    val mDatabase: UserDAO
+) : GithubRepository {
 
     override suspend fun getUserList(param: String): RequestHandler = networking.gitUserList(param)
 
     override suspend fun getUserDetail(login: String): RequestHandler = networking.getUserDetail(login)
+
+    override suspend fun getFavoriteList(): RequestHandler {
+        return try {
+            mDatabase.getFavorites()
+                .run {
+                    RequestHandler.Success(this)
+                }
+        }catch (ex: Exception){
+            RequestHandler.Failure(Exception("Falha ao buscar lista de favoritos"))
+        }
+    }
 }
