926754b49aaaa86ddfc0d232073de2b612e80a14
Daniel Eykel <danielbh609@hotmail.com>
2025-05-17 11:44:55 -0300

    feat: Implement session caching using SharedPreferences
    - Create `Session` data class to hold user and login status.
    - Define `SessionCache` interface with methods for saving, retrieving, and clearing sessions.
    - Implement `SessionCacheImpl` using `SharedPreferences` and `Moshi` for JSON serialization/deserialization.
    - Update `AulaAndroidModule` to provide `SharedPreferences` and `SessionCache` instances.
    - Inject `SessionCache` into `LoginNetworkingImpl` and `NewAccountNetworkingImpl`.

--- app/src/main/java/br/com/aulaandroid/data/model/UserModel.kt
+++ app/src/main/java/br/com/aulaandroid/data/model/UserModel.kt
@@ -1,6 +1,7 @@
 package br.com.aulaandroid.data.model
 
 import android.os.Parcelable
+import br.com.aulaandroid.ui.aulas.aula2.User
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -9,5 +10,19 @@ data class UserModel(
     val birthDay: String,
     val email: String,
     val password: String?
-) : Parcelable
+) : Parcelable {
+    companion object {
+        fun getUserModel(
+            name: String = "",
+            birthDay: String = "",
+            email: String = "",
+            password: String = ""
+        ) = UserModel(
+            name = name,
+            birthDay = birthDay,
+            email = email,
+            password = password
+        )
+    }
+}
 
